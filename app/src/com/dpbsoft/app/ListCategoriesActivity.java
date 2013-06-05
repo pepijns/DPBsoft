e29e8927881efeb61fceaa630f76e746635d3278
Pepijn Stroo <pepijnstroo@gmail.com>
2013-06-05 12:29:33 +0200

    Fix import

--- app/src/com/dpbsoft/app/ListCategoriesActivity.java
+++ app/src/com/dpbsoft/app/ListCategoriesActivity.java
@@ -7,8 +7,6 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
  
 public class ListCategoriesActivity extends ListActivity {
