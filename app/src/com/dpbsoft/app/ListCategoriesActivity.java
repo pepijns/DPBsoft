b269255c73cfa0b7dfa1b4cd96c0d8404a07c6da
Git <d-un_p@hotmail.com>
2013-06-06 12:27:58 +0200

    back

--- app/src/com/dpbsoft/app/ListCategoriesActivity.java
+++ app/src/com/dpbsoft/app/ListCategoriesActivity.java
@@ -11,8 +11,9 @@ import android.widget.AdapterView.OnItemClickListener;
  
 public class ListCategoriesActivity extends ListActivity {
  
-	static final String[] CATEGORIES = new String[] { "Dieren", "Natuur en milieu", "Noodhulp",
-			"Ontwikkelingshulp", "Vluchtelingenhulp", "Ziekten en aandoeningen", "Overig" };
+	static final String[] CATEGORIES = new String[] { "Dieren", "Categorie 2", "Categorie 3",
+			"Categorie 4", "Categorie 5", "Categorie 6", "Categorie 7", "Categorie 8",
+			"Categorie 9", "Categorie 10", "Categorie 11", "Categorie 12", "Categorie 13" };
  
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
